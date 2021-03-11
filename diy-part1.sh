e5f7c8ffd4172d7c0257a85c2fb035ddf0e16cd7
pop098 <65938943+pop098@users.noreply.github.com>
2021-03-11 09:20:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,3 @@
 git clone https://github.com/kenzok8/openwrt-packages.git  package/openwrt-packages
 git clone https://github.com/kenzok8/small.git  package/openwrt-small
 
-git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
-svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
